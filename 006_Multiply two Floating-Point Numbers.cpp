be5061da8073ea367c5b409209c98654be6733bb
Saiful-Islam-Shamim <saiful242955@gmail.com>
2025-08-20 00:23:11 +0600

    Description

---
+++ 006_Multiply two Floating-Point Numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    float x=23.00, y=34.30;
+    float multiplies = x*y;
+    cout << multiplies;
+    getch();
+}
---
+++ Print the ASCII Value of a Character.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    char cha;
+    cout << "Enter a character: ";
+    cin >> cha;
+    cout << "The ASCII value is " << int(cha);
+
+    getch();
+}
