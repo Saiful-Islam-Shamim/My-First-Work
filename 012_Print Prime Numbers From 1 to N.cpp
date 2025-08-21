de9ee64682b6c332ab4235b61577e01190b9e550
Saiful-Islam-Shamim <saiful242955@gmail.com>
2025-08-22 00:34:55 +0600

    Discription

---
+++ 012_Print Prime Numbers From 1 to N.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+#include<cmath>
+using namespace std;
+
+int main()
+ {
+        int n;
+        cout << "Enter n: ";
+        cin >> n;
+        for(int i=2; i<=n; i++){
+
+            bool prime = true;
+            for(int j=2; j <=sqrt(i); j++){
+
+                if(i %j == 0){
+                    prime = false;
+                    break;
+                }
+                if (prime) cout << i << " ";
+            }
+
+
+        }
+     getch();
+ }
+
