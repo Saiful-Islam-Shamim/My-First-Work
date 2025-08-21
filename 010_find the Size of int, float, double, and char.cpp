31abc68314303b491765a7a80ed30b540023f24b
Saiful-Islam-Shamim <saiful242955@gmail.com>
2025-08-22 00:09:23 +0600

    Discription

---
+++ 010_find the Size of int, float, double, and char.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+#include<math.h>
+int main()
+ {
+    int a;
+    float b;
+    double c;
+    char ch;
+
+    int e = sizeof(a);
+    cout << e;
+
+     getch();
+ }
+
+
+
+
