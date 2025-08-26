000ed3e52fef14ee570e6b06c8778b5698565516
Saiful-Islam-Shamim <saiful242955@gmail.com>
2025-08-26 22:16:09 +0600

    Discription

---
+++ 026_Print Fibonacci Series.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int fib(int n)
+{
+   if(n<=1)
+    return n;
+    return fib(n-1)+ fib(n-2);
+
+}
+int main()
+{
+    int n;
+    cout << "Enter the number of terms of fibonacci : ";
+    cin >> n;
+
+    for(int i = 0; i< n; i++){
+        cout << fib(i)<< "  ";
+    }
+    getch();
+}
